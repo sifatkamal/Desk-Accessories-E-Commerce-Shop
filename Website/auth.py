51381a3afd5473e886a24f548e91b41d2b7879c1
Md. Sifat Kamal <md.sifat.kamal@g.bracu.ac.bd>
2022-12-02 03:29:44 +0600

    auth file changed

--- Website/auth.py
+++ Website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 
 def login():
 
